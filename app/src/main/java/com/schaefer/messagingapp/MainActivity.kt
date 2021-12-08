046ffe278844e03d8574596a7f13f8d93d8514f8
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-12-08 10:35:31 +0100

    feat: simple navigation to MessagingList module

--- app/src/main/java/com/schaefer/messagingapp/MainActivity.kt
+++ app/src/main/java/com/schaefer/messagingapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.schaefer.messagingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.schaefer.messagingapp.databinding.ActivityMainBinding
+import com.schaefer.navigation.MessagingListNavigation
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val messagingListNavigation: MessagingListNavigation by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.helloWolrd.setOnClickListener {
+            messagingListNavigation.openMessagingList(this)
+            finish()
+        }
     }
 }
