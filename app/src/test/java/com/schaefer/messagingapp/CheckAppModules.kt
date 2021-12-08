b1b1a7e0d4ffbc018ccdf2e46b0f0b1a64671553
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-12-09 00:09:43 +0100

    fix: change androidContext and mockProvider to test dependencies

--- app/src/test/java/com/schaefer/messagingapp/CheckAppModules.kt
+++ app/src/test/java/com/schaefer/messagingapp/CheckAppModules.kt
@@ -1,18 +1,40 @@
 package com.schaefer.messagingapp
 
+import androidx.test.core.app.ApplicationProvider
 import com.schaefer.messagingapp.di.appModules
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.koin.dsl.koinApplication
+import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
 import org.koin.test.check.checkModules
+import org.koin.test.mock.MockProviderRule
+import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
 class CheckAppModules : KoinTest {
+
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        Mockito.mock(clazz.java)
+    }
+
+    @Before
+    fun setup() {
+        stopKoin()
+    }
 
     @Test
     fun verifyKoinApp() {
-        koinApplication {
+        startKoin {
+            androidContext(ApplicationProvider.getApplicationContext())
             modules(appModules)
-            checkModules()
-        }
+        }.checkModules()
     }
 }
