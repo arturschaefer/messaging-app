b42b14d5f8066182867506681b24f96c8f6b0753
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-12-09 13:17:09 +0100

    feat: adjust start of fragment and methods on viewModel

--- features/messagelist/src/main/java/com/schaefer/messagelist/presentation/MessageListFragment.kt
+++ features/messagelist/src/main/java/com/schaefer/messagelist/presentation/MessageListFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.schaefer.messagelist.databinding.MessagingListFragmentBinding
 import com.schaefer.messagelist.presentation.adapter.MessageListAdapter
+import com.schaefer.messagelist.presentation.model.MessageListVO
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 internal class MessageListFragment : Fragment() {
 
     private var _binding: MessagingListFragmentBinding? = null
     private val binding get() = _binding!!
-    private val messageListViewModel: MessageListViewModel by viewModel()
+
+    // TODO receive this value from argument or sharedViewModel
+    private val messageListViewModel: MessageListViewModel by viewModel {
+        parametersOf(MessageListVO("1234"))
+    }
     private val messageListAdapter = MessageListAdapter()
 
     override fun onCreateView(
@@ -32,10 +39,39 @@ internal class MessageListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val list = messageListViewModel.getMessageList()
+        setupRecyclerView()
+        setupListeners()
+        setupObservers()
+    }
+
+    private fun setupListeners() {
+        binding.includeSendMessageLayout.apply {
+            buttonSendMessage.setOnClickListener {
+                messageListViewModel.sendMessage("1234", this.editTextSendMessage.text.toString())
+                editTextSendMessage.setText("")
+            }
+            buttonSendMessage.setOnLongClickListener { messageListViewModel.sendMessages() }
+            editTextSendMessage.doAfterTextChanged {
+                val pair = messageListViewModel.validateEditText(it.toString())
+                buttonSendMessage.apply {
+                    isClickable = pair.first
+                    isEnabled = pair.first
+                    alpha = pair.second
+                }
+            }
+        }
+    }
+
+    private fun setupRecyclerView() {
         binding.recyclerViewMessages.apply {
             adapter = messageListAdapter
         }
-        messageListAdapter.messageList = list
+    }
+
+    private fun setupObservers() {
+        messageListViewModel.messageList.observe(viewLifecycleOwner) {
+            messageListAdapter.messageList = it
+            binding.recyclerViewMessages.smoothScrollToPosition(it.size)
+        }
     }
 }
