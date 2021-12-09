7fa66c9028f3f8aa7e3e0b20947a92c533fa5fae
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-12-09 13:15:43 +0100

    feat: adjust domain models

--- features/messagelist/src/main/java/com/schaefer/messagelist/domain/model/MessageText.kt
+++ features/messagelist/src/main/java/com/schaefer/messagelist/domain/model/MessageText.kt
@@ -2,7 +2,8 @@ package com.schaefer.messagelist.domain.model
 
 internal data class MessageText(
     val sendBy: String,
+    val chatId: String,
     val message: String,
-    val utcTime: String,
-    val messageStatus: MessageStatus = MessageStatus.HIDDEN
+    val time: Long,
+    val messageStatus: MessageStatus? = MessageStatus.HIDDEN
 )
